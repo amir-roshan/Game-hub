d394ab8dfa7435deb57ad224dd45a89979df2d97
amir-roshan <amirvalielahi@gmail.com>
2023-11-02 23:30:57 -0700

    Build game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,11 @@
+import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
 
-
-interface Game {
+export interface Game {
     id: number;
     name: string;
+    background_image: string;
 }
 
 interface FetchGamesResponse {
@@ -13,13 +13,13 @@ interface FetchGamesResponse {
     results: Game[];
 }
 
-
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
 
     useEffect(() => {
         const controller = new AbortController();
+
         apiClient
             .get<FetchGamesResponse>("/games", { signal: controller.signal })
             .then((res) => setGames(res.data.results))
@@ -28,8 +28,9 @@ const useGames = () => {
                 setError(err.message);
             });
 
-        return controller.abort();
+        return () => controller.abort();
     }, []);
+
     return { games, error };
 };
 
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: 'bdbe400d85884216ac0d10a4f0a4a54c'
+        key: '86c9e3d97736454a966c587d7a258d5d'
     }
 });
